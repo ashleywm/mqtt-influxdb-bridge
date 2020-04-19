9b3ec12f9d8071516b06f0ef754390e9b5c5bc4d
Ashley Morris <info@ashleywm.co.uk>
2020-04-19 16:23:50 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# MQTT Influxdb Bridge
+
+Node.js implementation of a MQTT to Influxdb bridge for recording sensor data transmitted over MQTT to InfluxDB
+
+## Installation
+
+Yarn
+
+```
+yarn install
+yarn run dev
+```
+
+npm 
+```
+npm install
+npm run dev
+```
+
+## Usage
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+
+## License
+
+MIT 
---
+++ index.js
@@ -0,0 +1,84 @@
+const mqtt = require('mqtt');
+const consola = require('consola');
+const Influx = require('influx');
+
+const influxOptions = {
+  host: process.env.INFLUX_HOST,
+  port: process.env.INFLUX_PORT || 8086,
+  username: process.env.INFLUX_USERNAME,
+  password: process.env.INFLUX_PASSWORD,
+  database: process.env.INFLUX_DB_NAME,
+};
+
+const mqttOptions = {
+  host: process.env.MQTT_HOST,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  protocol: process.env.MQTT_PROTOCOL || 'tls',
+};
+
+
+const influx = new Influx.InfluxDB({
+  ...influxOptions,
+  schema: [
+    {
+      measurement: 'parameter',
+      fields: {
+        value: Influx.FieldType.FLOAT,
+      },
+      tags: [
+        'topic',
+      ],
+    },
+  ],
+});
+
+
+const processMessage = (topic, message) => {
+  const data = [];
+
+  Object.keys(message).forEach((measurement) => {
+    const value = message[measurement];
+    data.push(
+      {
+        measurement,
+        tags: { topic },
+        fields: { value },
+      },
+    );
+  });
+
+  return data;
+};
+
+
+const topics = process.env.MQTT_TOPICS.split(',');
+
+if (!topics.length) {
+  consola.error('No topics to subscribe to');
+}
+
+const client = mqtt.connect(mqttOptions);
+
+client.subscribe(topics);
+
+client.on('connect', () => {
+  consola.success('Connected to MQTT broker');
+});
+
+client.on('offline', () => {
+  consola.warn('MQTT broker connection failed');
+});
+
+client.on('error', (error) => {
+  consola.error('MQTT Client Error:', error);
+});
+
+client.on('message', (topic, message) => {
+  const messageString = message.toString();
+  const parsedMessage = JSON.parse(messageString);
+
+  influx.writePoints(processMessage(topic, parsedMessage)).catch((err) => {
+    consola.error(`Error saving data to InfluxDB! ${err.stack}`);
+  });
+});
