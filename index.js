71a182912bb14d1842dcc9b8240da2d2c89e68f3
Ashley Morris <ashleywm@outlook.com>
2020-05-25 15:40:06 +0100

    Forcing process exit
    - This will allow docker to restart gracefully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const mqtt = require('mqtt');
-const consola = require('consola');
-const Influx = require('influx');
+const mqtt = require("mqtt");
+const consola = require("consola");
+const Influx = require("influx");
 
 const influxOptions = {
   host: process.env.INFLUX_HOST,
@@ -14,67 +14,62 @@ const mqttOptions = {
   host: process.env.MQTT_HOST,
   username: process.env.MQTT_USERNAME,
   password: process.env.MQTT_PASSWORD,
-  protocol: process.env.MQTT_PROTOCOL || 'tls',
+  protocol: process.env.MQTT_PROTOCOL || "tls",
 };
-
 
 const influx = new Influx.InfluxDB({
   ...influxOptions,
   schema: [
     {
-      measurement: 'parameter',
+      measurement: "parameter",
       fields: {
         value: Influx.FieldType.FLOAT,
       },
-      tags: [
-        'topic',
-      ],
+      tags: ["topic"],
     },
   ],
 });
-
 
 const processMessage = (topic, message) => {
   const data = [];
 
   Object.keys(message).forEach((measurement) => {
     const value = message[measurement];
-    data.push(
-      {
-        measurement,
-        tags: { topic },
-        fields: { value },
-      },
-    );
+    data.push({
+      measurement,
+      tags: { topic },
+      fields: { value },
+    });
   });
 
   return data;
 };
 
-
-const topics = process.env.MQTT_TOPICS.split(',');
+const topics = process.env.MQTT_TOPICS.split(",");
 
 if (!topics.length) {
-  consola.error('No topics to subscribe to');
+  consola.error("No topics to subscribe to");
 }
 
 const client = mqtt.connect(mqttOptions);
 
 client.subscribe(topics);
 
-client.on('connect', () => {
-  consola.success('Connected to MQTT broker');
+client.on("connect", () => {
+  consola.success("Connected to MQTT broker");
 });
 
-client.on('offline', () => {
-  consola.warn('MQTT broker connection failed');
+client.on("offline", () => {
+  consola.warn("MQTT broker connection failed");
+  process.exit(1);
 });
 
-client.on('error', (error) => {
-  consola.error('MQTT Client Error:', error);
+client.on("error", (error) => {
+  consola.error("MQTT Client Error:", error);
+  process.exit(1);
 });
 
-client.on('message', (topic, message) => {
+client.on("message", (topic, message) => {
   const messageString = message.toString();
   const parsedMessage = JSON.parse(messageString);
 
